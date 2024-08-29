97a278fb4c5a82c1f17c499e6cdb42af6881bb1c
David R <davidalexrodriguez@hotmail.com>
2024-08-28 20:49:33 -0600

    Add tests for execute function in MinimalAccount contract

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -17,6 +17,7 @@ contract HelperConfig is Script{
     uint256 constant ZKSYNC_SEPOLIA_CHAIN_ID = 300;
     uint256 constant LOCAL_CHAIN_ID = 31337;
     address constant BURNER_WALLET = 0xe68560ed18E28052242de35f2AA7fE522Bb7b2FC;
+    address constant FOUNDRY_DEFAULT_WALLET = 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38;
 
     NetworkConfig public localNetworkConfig;
     mapping(uint256 chainId => NetworkConfig) public networkConfigs;
@@ -52,5 +53,7 @@ contract HelperConfig is Script{
         if(localNetworkConfig.account != address(0)){
             return localNetworkConfig;
         }
+
+        return NetworkConfig({entryPoint: address(0), account: FOUNDRY_DEFAULT_WALLET});
     }
 }
